7deba2cbee8546174fe4d882c17a3ba6b8813e7e
Vladimir Keleshev <halst@ya.ru>
2013-05-30 20:28:36 +0200

    Rough prototype of Grammar class

--- peg.rb
+++ peg.rb
@@ -1,71 +1,114 @@
-class Literal
+class Expression
+  def name(value=nil)
+    if value
+      @name = value
+      self
+    else
+      @name
+    end
+  end
+
+  def result(text, children=nil)
+    res = {text: text}
+    children ? res.merge!({children: children}) : res
+    @name ? res.merge!({name: @name}) : res
+  end
+
+  def inspect
+    repr = "#{self.class}.new(#{self._inspect})"
+    @name ? repr + ".name(#{@name.inspect})" : repr
+  end
+
+  def ==(other)
+    self.inspect == other.inspect
+  end
+end
+
+class Literal < Expression
   def initialize(literal)
     @literal = literal
   end
 
   def match(text)
-    text.start_with?(@literal) ? @literal.length : nil
+    text.start_with?(@literal) ? result(@literal) : nil
+  end
+
+  def _inspect
+    @literal.inspect
   end
 end
 
 class Regex < Literal
   def match(text)
-    match = Regexp.new('^' + @literal).match(text)
-    match && match.to_s.length
+    res = Regexp.new('\A' + @literal).match(text)
+    res && result(res.to_s)
   end
 end
 
-class Sequence
+class Sequence < Expression
+  attr_accessor :children
+
   def initialize(*children)
     @children = children
   end
 
   def match(text)
-    length = 0
+    text_ = String.new(text)
+    len = 0
+    children = []
     @children.each do |member|
-      match = member.match(text)
-      if match == nil
+      res = member.match(text_)
+      if res == nil
         return nil
       else
-        text = text.slice match..text.length
-        length += match
+        children << res
+        text_ = text_.slice res[:text].length..text_.length
+        len += res[:text].length
       end
     end
-    length
+    result(text.slice(0...len), children)
+  end
+
+  def _inspect
+    @children.map(&:inspect).join(', ')
   end
 end
 
 class Or < Sequence
   def match(text)
     @children.each do |member|
-      match = member.match(text)
-      return match if match
+      res = member.match(text)
+      return result(res[:text], [res]) if res
     end
     nil
   end
 end
 
-class Not
+class Not < Expression
   def initialize(child)
     @child = child
   end
 
   def match(text)
-    @child.match(text) ? nil : 0
+    @child.match(text) ? nil : result('')
+  end
+
+  def _inspect
+    @child.inspect
   end
 end
 
 class And < Not
   def match(text)
-    @child.match(text) ? 0 : nil
+    @child.match(text) ? result('') : nil
   end
 end
 
-class Optional < Not
-  def match(text)
-    @child.match(text) || 0
-  end
-end
+#class Optional < Not
+#  def match(text)
+#    @child.match(text) || result('')
+#  end
+#end
 
 class OneOrMore < Not
   def range
@@ -73,22 +116,248 @@ class OneOrMore < Not
   end
 
   def match(text)
-    length = 0
-    count = 0
+    text_ = String.new(text)
+    len = 0
+    children = []
     loop do
-      match = @child.match(text)
-      break if not match
-      count += 1
-      break if match == 0
-      text = text.slice match..text.length
-      length += match
+      res = @child.match(text_)
+      break if not res
+      children << res
+      break if res[:text] == ''
+      text_ = text_.slice res[:text].length..text_.length
+      len += res[:text].length
     end
-    range.include?(count) ? length : nil
+    range.include?(children.length) ? result(text.slice(0...len),
+                                             children) : nil
   end
 end
 
 class ZeroOrMore < OneOrMore
   def range
     (0..Float::INFINITY)
+  end
+end
+
+class Optional < OneOrMore
+  def range
+    (0..1)
+  end
+end
+
+class Rule < Expression
+  attr_reader :reference
+
+  def initialize(name)
+    @reference = name
+  end
+
+  def _inspect
+    @reference.inspect
+  end
+end
+
+class Grammar
+  attr_reader :_grammar
+
+  def initialize(source)
+    @_grammar = node = self.class._peg.match(source)
+    if @_grammar[:text].length != source.length
+      raise SyntaxError.new source[@_grammar[:text].length, 50].inspect
+    end
+  end
+
+  def _parse(node)
+    return node if node[:name] == nil
+    self.send('visit_' + node[:name], node,
+              node[:children] ? node[:children].map {|c| _parse(c)} : nil)
+  end
+
+  def grammar
+    self._parse(@_grammar)
+  end
+
+  def visit_identifier__regex(node, children)
+    node[:text]
+  end
+
+  def visit_identifier(node, children)
+    identifier_regex, spacing = children
+    Rule.new(identifier_regex)
+  end
+
+  def visit_literal(node, children)
+    Literal.new(eval(node[:text]))
+  end
+
+  def visit_dot(node, children)
+    Regex.new('.')
+  end
+
+  def visit_class(node, children)
+    class_, spacing = children
+    Regex.new(class_[:text])
+  end
+
+  def visit_definition(node, children)
+    identifier, left_arrow, expression = children
+    expression.name(identifier.reference)
+  end
+
+  def visit_expression(node, children)
+    sequence, rest = children
+    if rest.length == 0
+      sequence
+    else
+      Or.new(sequence, *rest)
+    end
+  end
+
+  def visit_expression__zeroormore(node, children)
+    children
+  end
+
+  def visit_expression__sequence(node, children)
+    slash, sequence = children
+    sequence
+  end
+
+  def visit_grammar(node, children)
+    spacing, definitions = children
+    definitions
+  end
+
+  def visit_grammar__oneormore(node, children)
+    children
+  end
+
+  def visit_primary(node, children)
+    children[0]
+  end
+
+  def visit_primary__sequence(node, children)
+    identifier, not_left_arrow = children
+    identifier
+  end
+
+  def visit_primary__parens(node, children)
+    open, expression, close = children
+    expression
+  end
+
+  def visit_prefix__optional(node, children)
+    node[:text].strip  # HACK
+  end
+
+  def visit_prefix(node, children)
+    prefix, suffix = children
+    prefix == '' ? suffix : {'&' => And, '!' => Not}.fetch(prefix).new(suffix)
+  end
+
+  def visit_sequence(node, children)
+    children.length == 1 ? children[0] : Sequence.new(*children)
+  end
+
+  def visit_suffix__optional(node, children)
+    node[:text].strip  # HACK
+  end
+
+  def visit_suffix(node, children)
+    primary, optional_suffix = children
+    optional_suffix == '' ? primary : {
+      '?' => Optional,
+      '*' => ZeroOrMore,
+      '+' => OneOrMore,
+    }.fetch(optional_suffix).new(primary)
+  end
+
+  def self._peg
+    end_of_line = Or.new(
+                    Literal.new("\r\n"),
+                    Literal.new("\n"),
+                    Literal.new("\r"),
+                  )
+    space = Or.new(Literal.new(" "), Literal.new("\t"), end_of_line)
+    comment = Sequence.new(
+                Literal.new('#'),
+                ZeroOrMore.new(
+                  Sequence.new(Not.new(end_of_line), Regex.new('.')),
+                ),
+                end_of_line,
+              )
+    spacing = ZeroOrMore.new(Or.new(space, comment))
+
+    and_ = Sequence.new(Literal.new('&'), spacing)
+    not_ = Sequence.new(Literal.new('!'), spacing)
+    slash = Sequence.new(Literal.new('/'), spacing)
+    left_arrow = Sequence.new(Literal.new('<-'), spacing)
+    question = Sequence.new(Literal.new('?'), spacing)
+    star = Sequence.new(Literal.new('*'), spacing)
+    plus = Sequence.new(Literal.new('+'), spacing)
+    open = Sequence.new(Literal.new('('), spacing)
+    close = Sequence.new(Literal.new(')'), spacing)
+    dot = Sequence.new(Literal.new('.'), spacing).name('dot')
+
+    # HACK these three rules are simplified
+    literal = Sequence.new(Regex.new("'.*?'"), spacing).name('literal')
+    class_ = Sequence.new(Regex.new('\[.*?\]'), spacing).name('class')
+    identifier = Sequence.new(
+                   Regex.new('[A-Za-z0-9_]+').name('identifier__regex'),
+                   spacing
+                 ).name('identifier')
+
+    primary = Or.new(
+                Sequence.new(
+                  identifier,
+                  Not.new(left_arrow)
+                ).name('primary__sequence'),
+                Sequence.new(
+                  open,
+                  'EXPRESSION',  # paceholder for future substitution
+                  close
+                ).name('primary__parens'),
+                literal,
+                class_,
+                dot,
+              ).name('primary')
+    suffix = Sequence.new(
+               primary,
+               Optional.new(
+                 Or.new(question, star, plus)
+               ).name('suffix__optional'),
+             ).name('suffix')
+    prefix = Sequence.new(
+               Optional.new(
+                 Or.new(and_, not_)
+               ).name('prefix__optional'),
+               suffix
+             ).name('prefix')
+    sequence = ZeroOrMore.new(prefix).name('sequence')
+    expression = Sequence.new(
+                   sequence,
+                   ZeroOrMore.new(
+                     Sequence.new(slash, sequence).name('expression__sequence')
+                   ).name('expression__zeroormore')
+                 ).name('expression')
+    if primary.children[1].children[1] != 'EXPRESSION'
+      raise 'Invalid PEG grammar'
+    else
+      primary.children[1].children[1] = expression
+    end
+    definition = Sequence.new(
+                   identifier,
+                   left_arrow,
+                   expression
+                 ).name('definition')
+    # In the original PEG paper `grammar` is specified as:
+    #     grammar <- spacing definition+ end_of_file
+    # but we skip `end_of_file` allowing the grammar to
+    # match just a part of source in order to know where
+    # the syntax error occured.
+    grammar = Sequence.new(
+                spacing,
+                OneOrMore.new(definition).name('grammar__oneormore')
+              ).name('grammar')
+
+    grammar
   end
 end
--- test_peg.rb
+++ test_peg.rb
@@ -3,54 +3,141 @@ require './peg.rb'
 
 describe 'peg' do
   it 'literal' do
-    Literal.new('aaa').match('aaa...').should == 3
+    Literal.new('aaa').match('aaa...').should == {text: 'aaa'}
     Literal.new('aaa').match('......').should == nil
   end
 
   it 'regex' do
-    Regex.new('a*').match('aaa...').should == 3
+    Regex.new('a*').match('aaa...').should == {text: 'aaa'}
     Regex.new('a+').match('......').should == nil
   end
 
   it 'sequence' do
     grammar = Sequence.new(Regex.new('a*'), Literal.new('bbb'))
-    grammar.match('aaabbb...').should == 6
+    grammar.match('aaabbb...').should == {text: 'aaabbb',
+                                          children: [{text: 'aaa'},
+                                                     {text: 'bbb'}]}
     grammar.match('aaa......').should == nil
   end
 
   it 'or' do
     grammar = Or.new(Regex.new('a+'), Regex.new('b+'))
-    grammar.match('aabb').should == 2
-    grammar.match('bbb...').should == 3
+    grammar.match('aabb').should == {text: 'aa', children: [{text: 'aa'}]}
+    grammar.match('bbb...').should == {text: 'bbb', children: [{text: 'bbb'}]}
     grammar.match('...').should == nil
   end
 
   it 'not' do
-    Not.new(Regex.new('.')).match('').should == 0
+    Not.new(Regex.new('.')).match('').should == {text: ''}
     Not.new(Regex.new('.')).match('aa').should == nil
   end
 
   it 'and' do
-    And.new(Literal.new('a')).match('a').should == 0
+    And.new(Literal.new('a')).match('a').should == {text: ''}
     And.new(Literal.new('a')).match('b').should == nil
   end
 
   it 'optional' do
     grammar = Sequence.new(Optional.new(Literal.new('a')), Literal.new('b'))
-    grammar.match('ab').should == 2
-    grammar.match('b').should == 1
+    grammar.match('ab').should == {
+      text: 'ab', children: [{text: 'a', children: [{text: 'a'}]},
+                             {text: 'b'}]}
+    grammar.match('b').should == {
+      text: 'b', children: [{text: '', children: []},
+                            {text: 'b'}]}
   end
 
   it 'one or more' do
     grammar = OneOrMore.new(Literal.new('a'))
     grammar.match('.').should == nil
-    grammar.match('a.').should == 1
-    grammar.match('aaa.').should == 3
+    grammar.match('a.').should == {text: 'a', children: [{text: 'a'}]}
+    grammar.match('aaa.').should == {text: 'aaa', children: [{text: 'a'},
+                                                             {text: 'a'},
+                                                             {text: 'a'}]}
   end
 
   it 'zero or more' do
     grammar = ZeroOrMore.new(Literal.new('a'))
-    grammar.match('.').should == 0
-    grammar.match('aaa.').should == 3
+    grammar.match('.').should == {text: '', children: []}
+    grammar.match('aaa.').should == {text: 'aaa', children: [{text: 'a'},
+                                                             {text: 'a'},
+                                                             {text: 'a'}]}
+    ZeroOrMore.new(Regex.new('')).match('a').should == {text: '',
+                                                        children: [{text: ''}]}
+  end
+
+  it 'named' do
+    grammar = Literal.new('->').name('arrow')
+    grammar.name.should == 'arrow'
+    grammar.match('->').should == {name: 'arrow', text: '->'}
+  end
+end
+
+describe Grammar do
+  it 'literal' do
+    Grammar.new("rule<-'a'").grammar.should == [Literal.new('a').name('rule')]
+    Grammar.new("rule<-'b'/'c'").grammar.should == [Or.new(
+      Literal.new('b'), Literal.new('c')).name('rule')]
+    Grammar.new("rule_1<-'a'/rule_2\nrule_2<-'b'").grammar.should == [
+      Or.new(Literal.new('a'), Rule.new('rule_2')).name('rule_1'),
+      Literal.new('b').name('rule_2'),
+    ]
+  end
+
+  it 'has prefixes & and !' do
+    Grammar.new("rule1<-'a'/&'b' rule2<-'c'/!'d'").grammar.should == [
+      Or.new(Literal.new('a'), And.new(Literal.new('b'))).name('rule1'),
+      Or.new(Literal.new('c'), Not.new(Literal.new('d'))).name('rule2'),
+    ]
+  end
+
+  it 'has comments' do
+    Grammar.new("rule_1<-'a'/rule_2#comment\nrule_2<-'b'").grammar.should == [
+      Or.new(Literal.new('a'), Rule.new('rule_2')).name('rule_1'),
+      Literal.new('b').name('rule_2'),
+    ]
+  end
+
+  it 'has sequences' do
+    Grammar.new("rule <- 'a' !'b' rule2 rule2 <- 'hai'").grammar.should == [
+      Sequence.new(Literal.new('a'),
+                   Not.new(Literal.new('b')),
+                   Rule.new('rule2')).name('rule'),
+      Literal.new('hai').name('rule2'),
+    ]
+  end
+
+  it 'has suffixes ?, * and +' do
+    Grammar.new("rule <-'a'? 'b'* 'c'+").grammar.should == [
+      Sequence.new(Optional.new(Literal.new('a')),
+                   ZeroOrMore.new(Literal.new('b')),
+                   OneOrMore.new(Literal.new('c'))).name('rule')
+    ]
+  end
+
+  it 'has `.` that matches all' do
+    Grammar.new("rule <- 'a' !.").grammar.should == [
+      Sequence.new(Literal.new('a'), Not.new(Regex.new('.'))).name('rule')
+    ]
+  end
+
+  it 'has character classes like [a-z]' do
+    Grammar.new("rule <- [a-z] / [']").grammar.should == [
+      Or.new(Regex.new('[a-z]'), Regex.new("[']")).name('rule')
+    ]
+  end
+
+  it 'has grouping with (parenthesis)' do
+    Grammar.new("rule <- _ ('a' / 'b') _").grammar.should == [
+      Sequence.new(Rule.new('_'),
+                   Or.new(Literal.new('a'), Literal.new('b')),
+                   Rule.new('_')).name('rule')
+    ]
+  end
+
+  it 'raises SyntaxError on invalid syntax' do
+    expect do
+      Grammar.new("rule <- [a-z] %$@^")
+    end.to raise_error SyntaxError, "%$@^".inspect
   end
 end
